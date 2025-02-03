72e22324bde543618353a43af9978e4b4bcd5f8d
jessel <jesselclitar728@gmail.com>
2025-02-03 14:17:14 +0800

    first commit

---
+++ resources/views/jas.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jassy</title>
+</head>
+<body>
+    <h1>WELCOME MGA CUTIE</h1>
+</body>
+</html>
